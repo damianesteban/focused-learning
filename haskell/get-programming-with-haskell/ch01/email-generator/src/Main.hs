3ca1d710279db506c19b8bd0a054dcb84128f3da
Damian Esteban <damian.esteban@gmail.com>
2019-07-22 00:53:44 -0400

    Add refactored version of main function

--- haskell/get-programming-with-haskell/ch01/email-generator/src/Main.hs
+++ haskell/get-programming-with-haskell/ch01/email-generator/src/Main.hs
@@ -1,6 +1,6 @@
 messyMain :: IO()
 
-
+-- Original:
 messyMain = do
   print "Recipient?"
   recipient <- getLine
@@ -11,3 +11,28 @@ messyMain = do
   print ("Recipient: " ++ recipient ++ "\n" ++
           "Subject: " ++ subject ++ "\n" ++
           "Body: " ++ body ++ "\n")
+
+-- Refactored:
+recipientPart :: [Char] -> [Char]
+recipientPart recipient = "Dear " ++ recipient ++ ",\n"
+
+subjectPart :: [Char] -> [Char]
+subjectPart subject = subject
+
+bodyPart :: [Char] -> [Char]
+bodyPart body = body
+
+createEmail :: [Char] -> [Char] -> [Char] -> [Char]
+createEmail recipient subject body = recipientPart recipient ++
+                                     subjectPart subject ++
+                                     bodyPart body
+-- Final function
+main :: IO()
+main = do
+  print "Recipient?"
+  recipient <- getLine
+  print "Subject?"
+  subject <- getLine
+  print "Body?"
+  body <- getLine
+  print (createEmail recipient subject body)
